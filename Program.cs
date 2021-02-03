a96c8e2cc3c5516b9cf86f28f81696d37c949f74
YlaneT <yloo318@yahoo.fr>
2021-02-03 16:24:34 +0100

    Hello World en C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // Tell the program to wait for an input before closing
+            Console.ReadKey();
+        }
+    }
+}
